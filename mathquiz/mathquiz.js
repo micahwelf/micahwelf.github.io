10cf83140e94817fb43514834bf40ad84c02abd7
Micah Waddoups <dev@micahwelf.us>
2017-06-05 15:31:43 -0600

    auto update to mathquiz

--- mathquiz/mathquiz.js
+++ mathquiz/mathquiz.js
@@ -1,15 +1,17 @@
-console.log("Hello, console.");
-console.log(document.body);
+function changePage() {
+  console.log("Function on click activated.");
+  console.log(document.body);
 
-document.body.style.backgroundColor="darkgreen";
-document.body.style.fontFamily = "Arial";
+  document.body.style.backgroundColor="darkgreen";
+  document.body.style.fontFamily = "Arial";
 
-var x = 5;
+  var x = 5;
 
-var heading = document.querySelector("#title");
+  var heading = document.querySelector("#title");
 
-console.log(heading);
+  console.log(heading);
 
-heading.style.color = "blue";
+  heading.style.color = "blue";
 
-heading.innerHTML = "New Title set by JS";
+  heading.innerHTML = "New Title set by JS";
+}
