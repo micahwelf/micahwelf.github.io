b51ddf079726a22f06adc24444aacd7eeafbc038
Micah Waddoups <dev@micahwelf.us>
2017-06-05 15:08:19 -0600

    auto update to mathquiz

--- mathquiz/mathquiz.js
+++ mathquiz/mathquiz.js
@@ -1,4 +1,13 @@
 console.log("Hello, console.");
 console.log(document.body);
 
-document.body.style.backgroundColor="red";
+document.body.style.backgroundColor="darkgreen";
+document.body.style.fontFamily = "Arial";
+
+var x = 5;
+
+var heading = document.querySelector("#title");
+
+console.log(heading);
+
+heading.style.color = "blue";
