87d6d27b9ce7e220fbb05131fd5a02ae41b78311
Micah Waddoups <dev@micahwelf.us>
2017-06-06 15:23:53 -0600

    fill out XOs and make boxes click once only

--- grid_create/grid.js
+++ grid_create/grid.js
@@ -1,16 +1,26 @@
 var colorX = "rgba(255,0,0,0.9)";
 var colorO = "rgba(0,255,0,0.9)";
 var player = 0;
+var playerX = "playerX";
+var playerO = "playerO";
 var boxes = document.querySelectorAll(".square");
 boxes.forEach(function (square) {
   square.onclick = function () {
     console.log(square);
+    if (square.classList.contains("playerX")) {
+      return;
+    }
+    if (square.classList.contains("playerO")) {
+      return;
+    }
     if (player == 0) {
       square.classList.add("playerX");
+      square.innerHTML = "X";
       player = (player + 1) % 2;
     } else {
       if (player == 1) {
         square.classList.add("playerO");
+        square.innerHTML = "O";
         player = (player + 1) % 2;
       }
     }
