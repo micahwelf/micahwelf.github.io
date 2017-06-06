9bd65ad397d16c7491a1c4fc61234bb0ea491abb
Micah Waddoups <dev@micahwelf.us>
2017-06-06 16:52:31 -0600

    added 2048 game and updated style with tic-tac-toe

---
+++ 2048/game.js
@@ -0,0 +1,16 @@
+var tile = '<div class="tile"></div>';
+var newTile = document.createElement("div");
+newTitle.appendChild(tile);
+var mainBoard = document.querySelector("#board");
+
+for (var i = 0; i < 4; i++) {
+  var row = document.createElement("div");
+  row.classList.add("row");
+  mainBoard.appendChild(row);
+
+  for (var j = 0; j < 4; j++) {
+    var tile = document.createElement("div");
+    tile.classList.add("tile");
+  }
+
+}
