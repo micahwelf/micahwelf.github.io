ae8094a40707f6ef5f19d8727f781c944b47d941
Micah Waddoups <dev@micahwelf.us>
2017-06-07 10:30:52 -0600

    updated tic-tac-toe javascript

--- grid_create/grid.js
+++ grid_create/grid.js
@@ -4,25 +4,39 @@ var player = 0;
 var playerX = "playerX";
 var playerO = "playerO";
 var boxes = document.querySelectorAll(".square");
+var row_wrap = Math.sqrt(boxes.length);
+var rows = [];
+for (var i = 0; i < boxes.length; i++) {
+   if ((i % row_wrap) == 0) {
+      var r = [];
+   }
+   r[(i%row_wrap)] = boxes[i];
+   if (((i + 1) % row_wrap) == 0) {
+      rows[(i / row_wrap)] = r;
+   }
+}
 boxes.forEach(function (square) {
-  square.onclick = function () {
-    console.log(square);
-    if (square.classList.contains("playerX")) {
-      return;
-    }
-    if (square.classList.contains("playerO")) {
-      return;
-    }
-    if (player == 0) {
-      square.classList.add("playerX");
-      square.innerHTML = "X";
-      player = (player + 1) % 2;
-    } else {
-      if (player == 1) {
-        square.classList.add("playerO");
-        square.innerHTML = "O";
-        player = (player + 1) % 2;
+   square.onclick = function () {
+      console.log(square);
+      if (square.classList.contains("playerX")) {
+         return;
       }
-    }
-  };
+      if (square.classList.contains("playerO")) {
+         return;
+      }
+      if (player == 0) {
+         square.classList.add("playerX");
+         square.innerHTML = "X";
+         player = (player + 1) % 2;
+      } else {
+         if (player == 1) {
+            square.classList.add("playerO");
+            square.innerHTML = "O";
+            player = (player + 1) % 2;
+         }
+      }
+      for (var i = boxes.length; i < 0; i--) {
+
+      }
+   };
 });
