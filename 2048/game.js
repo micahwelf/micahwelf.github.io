9136d477f5c66ac81d0214ca5edfa434906799c6
Micah Waddoups <dev@micahwelf.us>
2017-06-07 13:57:34 -0600

    typo title to tile

--- 2048/game.js
+++ 2048/game.js
@@ -2,7 +2,7 @@ var board = {};
 
 var tile = '<div class="tile"></div>';
 var newTile = document.createElement("div");
-newTitle.appendChild(tile);
+newTile.appendChild(tile);
 
 var createBoard = function () {
   var mainBoard = document.querySelector("#board");
