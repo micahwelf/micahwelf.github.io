378e47fee47b415fd1b602ea26c0bd6f83427889
Micah Waddoups <dev@micahwelf.us>
2017-06-07 13:47:06 -0600

    add instructor files and functionize create board.

--- 2048/game.js
+++ 2048/game.js
@@ -1,16 +1,23 @@
 var tile = '<div class="tile"></div>';
 var newTile = document.createElement("div");
 newTitle.appendChild(tile);
-var mainBoard = document.querySelector("#board");
 
-for (var i = 0; i < 4; i++) {
-  var row = document.createElement("div");
-  row.classList.add("row");
-  mainBoard.appendChild(row);
+var createBoard = function () {
+  var mainBoard = document.querySelector("#board");
 
-  for (var j = 0; j < 4; j++) {
-    var tile = document.createElement("div");
-    tile.classList.add("tile");
+  for (var i = 0; i < 4; i++) {
+    var row = document.createElement("div");
+    row.classList.add("row");
+    mainBoard.appendChild(row);
+
+    for (var j = 0; j < 4; j++) {
+      var tile = document.createElement("div");
+      tile.classList.add("tile");
+    }
+
   }
 
 }
+
+
+creatBoard();
