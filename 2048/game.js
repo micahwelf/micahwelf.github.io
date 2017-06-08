ef55beff77439c68c24bca7d094e34b7680ab3f5
Micah Waddoups <dev@micahwelf.us>
2017-06-08 11:48:01 -0600

    working on key events...

--- 2048/game.js
+++ 2048/game.js
@@ -102,6 +102,32 @@ var assignValue = function (row, col, value) {
 };
 
 
+/* key events:
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+    }
+
+}
+
+*/
+
 createBoard();
 
 assignValue(1,1,2);
