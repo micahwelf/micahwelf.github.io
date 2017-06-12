85e4ea745fd87a1b4a6f3c083470735cf040a156
micahwelf <micahwelf@gmail.com>
2017-06-12 10:36:48 -0600

    genRand fix again

--- 2048/game.js
+++ 2048/game.js
@@ -217,10 +217,11 @@ var generateRandomTile = function () {
          }
       }
    }
-   if (emptyTiles.length > 0) {
-      for (var targetIndex; (targetIndex < emptyTiles.length && targetIndex >= 0); console.log(targetIndex)) {
-         targetIndex = ((Math.random() * emptyTiles.length) % emptyTiles.length).toFixed(0);
-      }
+   var max = emptyTiles.length;
+   if (max > 0) {
+      //for (var targetIndex; (targetIndex < emptyTiles.length && targetIndex >= 0); console.log(targetIndex)) {
+      targetIndex = (Math.random() * max).toFixed(0) % max;
+      //}
       var targetRow = (emptyTiles[targetIndex])[0];
       var targetCol = (emptyTiles[targetIndex])[1];
       var targetValue = newNumber[((Math.random() * newNumber.length) % newNumber.length).toFixed(0)];
