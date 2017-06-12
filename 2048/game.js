a22789731a46b68c8c363af6d571099d478e5d4b
micahwelf <micahwelf@gmail.com>
2017-06-12 08:51:42 -0600

    fix space and typo

--- 2048/game.js
+++ 2048/game.js
@@ -46,6 +46,11 @@ var createBoard = function () {
 
 }
 
+
+
+
+
+
 var toScore = function (name, score) {
    var newscore = {};
    newscore["name"] = name;
@@ -93,6 +98,11 @@ var saveScore = function () {
 var updateScore = function (add) {
    score += add;
 }
+
+
+
+
+
 
 var divCell = function (row,col) {
    var divBoard = document.querySelector("#game-board");
@@ -168,6 +178,7 @@ var setColNumbers = function(row, numbers) {
 //  direction ::=   "forward" | "backward"
 var combineNumbers = function (direction, numbers) {
    var newNumbers = [];
+
    if (direction === "forward") {
       while (numbers.length > 0) {
          if (numbers[0] === numbers[1]) {
@@ -218,7 +229,7 @@ var generateRandomTile = function () {
    }
    if (emptyTiles.length > 0) {
       targetIndex = ((Math.random() * emptyTiles.length) % emptyTiles.length).toFixed(0);
-      targetValue = newNumber[((Math.random() * newNumbers.length) % newNumbers.length).toFixed(0)];
+      targetValue = newNumber[((Math.random() * newNumber.length) % newNumbers.length).toFixed(0)];
       virtualBoard[emptyTiles[targetIndex]] = targetValue;
    } else {
       divMessage.innerHTML = "Board Full - Game Over";
